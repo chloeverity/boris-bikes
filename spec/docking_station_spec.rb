76c672ec654dee212ca814add76c51905d4e1c66
Reka <rekapap28@gmail.com>
2018-10-02 14:22:32 +0100

    adding step-8 fix

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,9 @@
 require 'DockingStation'
 
 describe DockingStation do
-  docking_station = DockingStation.new
-  it { docking_station.respond_to?(:release_bike) }
+  it 'creates new DockStation object' do
+    docking_station = DockingStation.new
+    expect(docking_station).instance_of?(DockingStation)
+  end
+  # it { expect(DockingStation.new.respond_to?(:release_bike) == true) }
 end
